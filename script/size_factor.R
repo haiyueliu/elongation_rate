87d924a5320d7f93a17262c088310f91d1a402b6
Haiyue Liu <haiyue_liu@outlook.com>
2026-03-20 11:18:34 +0100

    Fix typos and enhance argument handling in size_factor.R
    Fixed typos and improved argument handling in size_factor.R. Adjusted normalization and output formatting for size factors.

--- script/size_factor.R
+++ script/size_factor.R
@@ -2,13 +2,12 @@
 args = commandArgs(trailingOnly=TRUE)
 
 ### Arguments
-if (length(args)<3) {
+if (length(args) < 3) {
   stop("Not all arguments are supplied. Usage: size_factor.R <work.dir> <samplesheet> <gtf>\n", call. = FALSE)
-} 
-
-work.dir <- args[1]
+}
+work.dir   <- args[1]
 samplesheet <- args[2]
-gtf <- args[3]
+gtf        <- args[3]
 
 ### Libraries
 suppressPackageStartupMessages({
@@ -24,26 +23,30 @@ suppressPackageStartupMessages({
 
 ### Directories
 featureCounts.dir <- file.path(work.dir, "featureCounts")
-data.dir <- file.path(work.dir, "analysis")
+data.dir          <- file.path(work.dir, "analysis")
 
 ### Samplesheet
-sample.info <- read.table(samplesheet, header = TRUE, atringAsFactors = FALSE)
+# FIX: typo 'atringAsFactors' -> 'stringsAsFactors'
+sample.info <- read.table(samplesheet, header = TRUE, stringsAsFactors = FALSE)
 
-## Annotation
-gtf.dat  <- rtracklayer::import(gtf)
-gtf.dat  <- as.data.frame(gtf.dat)
+### Annotation
+gtf.dat <- rtracklayer::import(gtf)
+gtf.dat <- as.data.frame(gtf.dat)
 gtf.dat %<>% mutate(species = ifelse(seqnames %in% paste0("chr", c(seq(1, 22), "X", "Y", "M")), "human", "yeast"))
-gene.dat <- gtf.dat[gtf.dat$type %in% "gene",]
-cat("Number of annotated genes by species: \n")
+gene.dat <- gtf.dat[gtf.dat$type %in% "gene", ]
+cat("Number of annotated genes by species:\n")
 print(table(gene.dat$species))
 
 ### Read count data
-raw.data <- list()
+raw.data   <- list()
 count.data <- list()
-
-for (sample in sample.info$sample_name){
+for (sample in sample.info$sample_name) {
   print(sample)
-  raw.data[[sample]] <- read.table(file.path(featureCounts.dir, paste0(sample, ".gene.featureCounts.txt"), ), header = TRUE, stringAsFactors = FALSE)
+  # FIX: removed trailing comma inside file.path(); fixed typo 'stringAsFactors' -> 'stringsAsFactors'
+  raw.data[[sample]] <- read.table(
+    file.path(featureCounts.dir, paste0(sample, ".gene.featureCounts.txt")),
+    header = TRUE, stringsAsFactors = FALSE
+  )
   colnames(raw.data[[sample]]) <- c("gene_id", "chr", "start", "end", "strand", "length", "count")
   count.data[[sample]] <-
     raw.data[[sample]] %>%
@@ -55,36 +58,56 @@ count.data <- rbindlist(count.data)
 count.data %<>%
   pivot_wider(names_from = sample, values_from = count) %>%
   as.data.frame()
-
 rownames(count.data) <- count.data$gene_id
-count.data <- count.data[,-1]
+count.data <- count.data[, -1]
+
 cat("Number of genes in count data:", nrow(count.data), "\n")
 
-### filter genes that do not express in any samples
-count.data <- count.data[apply(count.data, 1, sum) > 0,]
-cat("Number of genes in count data after filtering genes with 0 counts across samples:", nrow(count.data), "\n")
+### Filter genes with 0 counts across all samples
+count.data <- count.data[apply(count.data, 1, sum) > 0, ]
+cat("Number of genes after filtering 0-count genes:", nrow(count.data), "\n")
 
-### spike-in ratio 
-spikein.count.data <- count.data[rownames(count.data) %in% gene.dat$gene_id[gene.dat$species!="human"], ]
-human.count.data <- count.data[rownames(count.data) %in% gene.dat$gene_id[gene.dat$species=="human"], ]
+### Spike-in vs human split
+spikein.count.data <- count.data[rownames(count.data) %in% gene.dat$gene_id[gene.dat$species != "human"], ]
+human.count.data   <- count.data[rownames(count.data) %in% gene.dat$gene_id[gene.dat$species == "human"], ]
+
+### Spike-in ratio (informational)
 count.ratio <-
-  data.frame(sample_id = sample.info$sample_id,
-             spikein = colSums(spikein.count.data) / colSums(count.data))
+  data.frame(
+    # FIX: use sample_name (matches count.data colnames) so the left_join key aligns
+    sample_name = sample.info$sample_name,
+    spikein     = colSums(spikein.count.data) / colSums(count.data)
+  )
+sample.info %<>% left_join(count.ratio, by = "sample_name")
 
-sample.info %<>% left_join(count.ratio)
+### DESeq2 size factors
+# FIX: rownames of colData must match colnames of countData (both are sample_name)
+sample.info$sample <- as.factor(sample.info$sample)   # FIX: design variable must be a factor
+rownames(sample.info) <- sample.info$sample_name
 
-### DESeq2
-#### size factor 
-meta.data <- sample.info
-rownames(meta.data) <- sample.info$sample_id
-dds <- 
-  DESeqDataSetFromMatrix(countData = as.matrix(count.data),
-                         colData = sample.info,
-                         design = ~ 0 + sample)
+dds <-
+  DESeqDataSetFromMatrix(
+    countData = as.matrix(count.data),
+    colData   = sample.info,
+    design    = ~ 0 + sample
+  )
 
-### normalization to the yeast spike-ins 
+### Normalize to yeast spike-ins
 dds <- estimateSizeFactors(dds, controlGenes = rownames(count.data) %in% rownames(spikein.count.data))
-cat("Normalization factors for each sample: \n")
-yeast_size_factor <- data.frame(sample_id = names(sizeFactors(dds)), size_factor=sizeFactors(dds))
-yeast_size_factor
-write.table(yeast_size_factor, paste0(data.dir, "size_factors_deseq2.txt"), quote=F, sep="\t", row.names =FALSE)
+
+cat("Normalization factors for each sample:\n")
+# FIX: use sample_name as column 1 (bash step 11 awk matches on sample_name, not sample_id)
+yeast_size_factor <- data.frame(
+  sample_name = names(sizeFactors(dds)),
+  size_factor = sizeFactors(dds)
+)
+print(yeast_size_factor)
+
+# FIX: use file.path() to avoid missing separator (paste0 would give .../analysissize_factors...)
+write.table(
+  yeast_size_factor,
+  file.path(data.dir, "size_factors_deseq2.txt"),
+  quote     = FALSE,
+  sep       = "\t",
+  row.names = FALSE
+)
